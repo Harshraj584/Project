999f74a37ddb385eb2bf6062620e4b10439f96a9
Harshraj584 <rajharshraj584@gmail.com>
2025-02-26 19:39:31 +0530

    Create Toolbar.js

---
+++ Toolbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Toolbar = () => {
+  return (
+    <div className="toolbar">
+      <button>Bold</button>
+      <button>Italic</button>
+      <button>Underline</button>
+    </div>
+  );
+};
+
+export default Toolbar;
