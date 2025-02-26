f883a67af8bc6d1f2c55100c564cee4b4b8d9152
Harshraj584 <rajharshraj584@gmail.com>
2025-02-26 20:21:25 +0530

    Update Toolbar.js

--- Toolbar.js
+++ Toolbar.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { trim, upper, lower } from "../dataQualityFunctions";
 
-const Toolbar = () => {
+const Toolbar = ({ grid, setGrid, selectedCell, handleCellChange }) => {
+  const handleFormat = (format) => {
+    const { row, col } = selectedCell;
+    const newGrid = [...grid];
+    newGrid[row][col] = format(grid[row][col]);
+    setGrid(newGrid);
+  };
+
   return (
     <div className="toolbar">
-      <button>Bold</button>
-      <button>Italic</button>
-      <button>Underline</button>
+      <button onClick={() => handleFormat((text) => `<b>${text}</b>`)}>Bold</button>
+      <button onClick={() => handleFormat((text) => `<i>${text}</i>`)}>Italic</button>
+      <button onClick={() => handleFormat(trim)}>Trim</button>
+      <button onClick={() => handleFormat(upper)}>UPPER</button>
+      <button onClick={() => handleFormat(lower)}>lower</button>
     </div>
   );
 };
